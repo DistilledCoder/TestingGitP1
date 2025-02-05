57c9718c29427a4358ab188d7503d1f554fd0b97
ericw <ericw@UncleRicco.san.rr.com>
2025-02-04 20:36:04 -0800

    struggling to get this right...

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -3,5 +3,14 @@ package demo1;
 public class DemoTest1 {
 	public static void main(String[] args) {
 		System.out.println("Demo Test 1!");
+		
+		int x = 34;
+		int y = 22;
+		
+		int result = x + y;
+		
+		
+			System.out.println(result);
+		
 }
 }
